d3a8ec0cc7cbb529734f896680caaf790e028322
zhangf@fescotech.com <zhangf@fescotech.com>
2019-09-17 18:48:53 +0800

    add microservice

---
+++ microservice-simple-consumer-movie/src/main/java/com/example/model/User.java
@@ -0,0 +1,13 @@
+package com.example.model;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+@Data
+public class User {
+    private Long id;
+    private String username;
+    private String name;
+    private Integer age;
+    private BigDecimal balance;
+}
